72849fc915d25da503e3c790af6f0cd763331f31
labreezy <yourtooslow@gmail.com>
2024-05-17 09:34:04 -0500

    second, updated object db among other things'

---
+++ bingo_egg_tierlist.py
@@ -0,0 +1,173 @@
+from glob import glob
+import json
+import re
+import igraph as ig
+import matplotlib.pyplot as plt
+
+LEVEL_REGEX = re.compile(r"obj_([a-z]+)([1-8])\.json")
+
+LONGNAMES = {
+    "green": "Forest",
+    "blue":"Pirate",
+    "red":"Dino",
+    "purple":"Blizzard",
+    "orange":"Circus",
+    "yellow":"Sand",
+    "last":"Palace"
+}
+
+WORLD_ORDER = ["green","blue","red","purple","orange","yellow","last"]
+EGG_NAMES = ["Spotted",
+"Fire Comb",
+            "Water Comb",
+            "Lightning Comb",
+            "Ice Comb",
+            "Wind Comb",
+            "Iron Comb",
+            "Light Comb",
+            "Wings",
+            "Booster",
+            "Paraloop",
+            "Thorn Egg",
+            "Speed Shoes",
+            "Bomb",
+            "Spring Shoes",
+            "Circus Hat",
+            "Psychic Hat",
+            "Heart Hat",
+            "Bat",
+            "Crow",
+            "Cipher",
+            "Clippen",
+            "Recky",
+            "Richie",
+            "Peliwan",
+            "Runny",
+            "Rabbish",
+            "Rikol",
+            "Kaboot",
+            "Datch",
+            "Glarin",
+            "Baskus",
+            "Oritta",
+            "Biboo",
+            "Gorilla",
+            "Chameleon",
+            "Mouse",
+            "Turtle",
+            "Lion",
+            "Dice",
+            "Super Fruit",
+            "Tiger",
+            "Sheep",
+            "Hawk",
+            "Fox",
+            "Large Butterfly",
+            "Stopwatch",
+            "Small Butterfly",
+            "1 Up",
+            "Chick Bomb",
+            "Egg Bomb",
+            "Game Boy Advance Game: Chu Chu Rocket Challenge",
+            "Game Boy Advance Game: Nights Score Attack",
+            "Game Boy Advance Game: Billy Hatcher Shoot - Easy",
+            "Game Boy Advance Game: Billy Hatcher Hyper Shoot",
+            "Game Boy Advance Game: Puyo Pop",
+            "Sonic",
+            "Tails",
+            "Knuckles",
+            "Chao",
+            "Rappy",
+            "Kapu Kapu",
+            "NIGHTS",
+            "Amigo",
+            "Super Clippen",
+            "Super Recky",
+            "Chicken Suit",
+            "Oma-Oma",
+            "Uri-Uri",
+            "Ura-Ura",
+            "Ponee",
+            "Allani",
+            "Mera-Mera"
+]
+
+
+class Level():
+    def __init__(self,world,mnum,egglist):
+        self.world = world
+        self.missionnum = mnum
+        self.egglist = egglist
+    def __str__(self):
+        return LONGNAMES[self.world] + " " + str(self.missionnum)
+
+
+
+
+def get_egglist_from_json(fn):
+    objlist = json.loads(open(fn).read())
+
+    eggs_full = [o for o in objlist['data'] if o['name'] == "egg"]
+    unique_eggs = list(set(list(map(lambda e: e["intparams"][1], eggs_full))))
+    unique_eggs.sort()
+    return [EGG_NAMES[i] for i in unique_eggs]
+if __name__ == '__main__':
+    edgelist = []
+    all_levels = []
+    for fn in glob("base_setjsons/*.json"):
+        groups = LEVEL_REGEX.findall(fn)
+        if len(groups) > 0:
+            world, mnum = groups[0]
+            if world == "test":
+                continue
+            lvl = Level(world,int(mnum),get_egglist_from_json(fn))
+            all_levels.append(lvl)
+            all_levels.sort(key=lambda l: WORLD_ORDER.index(l.world)*8+l.missionnum)
+    print(list(map(str, all_levels)))
+    for world in range(7):
+        #generic conditions
+        for m in range(4):
+            level_idx = world*8+m
+            edgelist.append((level_idx,level_idx+1))
+    for world in range(6):
+        edgelist.append((world*8+1,(world+1)*8))
+
+    #pirate 4 to forest/pirate 6
+    edgelist.append((11,5))
+    edgelist.append((11,13))
+    edgelist.append((11,21))
+    #dino 4 to forest,pirate,dino 7
+    edgelist.append((8*3+3,6))
+    edgelist.append((8*3+3, 8+6))
+    edgelist.append((8*3+3, 16+6))
+    #blizzard 4 to forest,pirate,dino,blizzard 8
+    edgelist.append((8*4+3, 7))
+    edgelist.append((8*4+3, 8+7))
+    edgelist.append((8*4+3, 16+7))
+    edgelist.append((8*4+3, 24+7))
+
+    #connect friend missions to after boss for blizzard, circus, sand
+    for j in range(3,6):
+        for i in range(5,8):
+            edgelist.append((j*8+1,j*8+i))
+    #palace too
+    for i in range(5,8):
+        edgelist.append((48,48+i))
+
+
+
+
+    fig, ax = plt.subplots()
+    g = ig.Graph(56, edgelist, directed=True)
+
+    ig.plot(g,target=ax,vertex_color="steelblue",vertex_size=10,vertex_label=list(map(str,all_levels)))
+    plt.show()
+
+
+
+
+
+
+
+
+
--- setbin_json.py
+++ setbin_json.py
@@ -6,20 +6,19 @@ import json
 from binascii import hexlify
 from glob import glob
 
+BILLY_DIR = r"C:\Users\yourt\Downloads\Billy Hatcher\\"
 SET_OBJ_SIZE = 0x10000
 
 setfn = sys.argv[1]
 fmt = ">I3f48s"
 empty_dat = b'\x00' * 0x40
 db = {}
+setfn =sys.argv[1]
 if "obj" in setfn:
     db = json.load(open('db/obj_db.json'))
     reverse_db = {v:k for k,v in db.items()}
-for fn in glob(r"C:\Users\yourt\Downloads\Billy Hatcher\set_obj_*.bin"):
-    if "_out" in fn:
-        continue
-    setfn = fn
-    print(setfn)
+
+for setfn in glob(osp.join(BILLY_DIR,"set_obj*.bin")):
     datalist = []
     if setfn.endswith('bin'):
         out_obj = {}
@@ -39,19 +38,21 @@ for fn in glob(r"C:\Users\yourt\Downloads\Billy Hatcher\set_obj_*.bin"):
                     currobj['name'] = db[str(objid)]
                 else:
                     currobj["name"] = "unk" + str(objid)
-
-                currobj["position"] = [x,y,z]
-                currobj['shortparams'] = list(struct.unpack('>4h',params[:8]))
+                currobj["position"] = [x,-z,y]
+                if objid == 0x24:
+                    print(currobj["position"])
+                currobj['shortparams'] = list(struct.unpack('>4H',params[:8]))
                 currobj['intparams'] = list(struct.unpack('>5i',params[8:0x1C]))
                 currobj['floatparams'] = list(struct.unpack('>5f',params[0x1C:]))
                 datalist.append(currobj)
         setfn = osp.split(setfn)[-1]
-        outfn = setfn[:-4] + ".json"
+        outfn = osp.join("base_setjsons", osp.splitext(setfn)[0] + ".json")
+        print("Outputting to " + outfn)
         out_obj['data'] = datalist
         json.dump(out_obj,open(outfn,"w"),indent=2)
-        print("IDs:")
+        """print("IDs:")
         print(list(set([o['name'] for o in datalist])))
-        print("done")
+        print("done")"""
     elif setfn.endswith("json"):
         objdata = json.load(open(setfn))
         base, ext = osp.splitext(setfn)
@@ -61,11 +62,13 @@ for fn in glob(r"C:\Users\yourt\Downloads\Billy Hatcher\set_obj_*.bin"):
                     id = int(obj['name'][3:])
                 else:
                     id = int(reverse_db[obj['name']])
-                x,y,z = obj['position']
-                extrabytes = struct.pack('>4h',*obj['shortparams'])
-                extrabytes = struct.pack('>5i',*obj['intparams'])
+                x,nz,y = obj['position']
+
+                extrabytes = struct.pack('>4H',*obj['shortparams'])
+                extrabytes += struct.pack('>5i',*obj['intparams'])
                 extrabytes += struct.pack('>5f', *obj['floatparams'])
-                binf.write(struct.pack(fmt,id,x,y,z,extrabytes))
+
+                binf.write(struct.pack(fmt,id,x,y,-nz,extrabytes))
             while binf.tell() < objdata['fsize']:
                 binf.write(empty_dat)
         print("Done")
